732c7c806de4e828a63746b0a41ffd32a1445bb9
Kim Lindberg Schwaner <kim.schwaner@gmail.com>
2011-11-29 14:15:44 +0100

    Updated .gitignore and removed project files from root dir

--- Default/tests/dll_test_main.d
+++
@@ -1,7 +0,0 @@
-tests/dll_test_main.d tests/dll_test_main.o: ../tests/dll_test_main.cpp \
-  /Users/mariannebondelarsen/Desktop/projekt/SoundComCode/include/dataLinkLayer.h \
-  /Users/mariannebondelarsen/Desktop/projekt/SoundComCode/include/frame.h
-
-/Users/mariannebondelarsen/Desktop/projekt/SoundComCode/include/dataLinkLayer.h:
-
-/Users/mariannebondelarsen/Desktop/projekt/SoundComCode/include/frame.h:
