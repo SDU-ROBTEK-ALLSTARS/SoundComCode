bd2c2943ae25cf45ddfd0fb8392bb14175db1cab
Rudi Hansen <dimx@dimx.dk>
2011-12-17 17:43:59 +0100

    DtmfInMessage updated

--- src/buffers/DtmfInMessage.cpp
+++ src/buffers/DtmfInMessage.cpp
@@ -1,16 +1,27 @@
 #include "DtmfInMessage.h"
 
-//DtmfInMessage::DtmfInMessage();
 DtmfInMessage::DtmfInMessage(unsigned char senderAddress, unsigned char senderPort, unsigned long dataLength, unsigned char * data) : senderAddress_(senderAddress), senderPort_(senderPort), dataLength_(dataLength), data_(data){}
 DtmfInMessage::~DtmfInMessage()
 {
 	delete(this->data_);
 }
-//unsigned char DtmfInMessage::getSenderAdress();
-//void DtmfInMessage::getData(unsigned char *& data, unsigned long startAddress, unsigned long stopAddress);
+unsigned char DtmfInMessage::getSenderAddress()
+{
+	return this->senderAddress_;
+}
+void DtmfInMessage::getData(unsigned char *& data, unsigned long startAddress, unsigned long stopAddress)
+{
+	// Valid address ?
+	if ((startAddress >= stopAddress) || ((stopAddress-startAddress) > this->dataLength_))
+		return;
+
+	// Copy data to provided pointer
+	std::memcpy(data, (this->data_+startAddress),(stopAddress-startAddress));
+}
 void DtmfInMessage::getData(unsigned char *& data)
 {
-	data = this->data_; 
+	// Copies all data to provided data area (Be aware to allocate enough space!)
+	std::memcpy(data, this->data_, this->dataLength_);
 }
 unsigned long DtmfInMessage::getMessageLength()
 {
