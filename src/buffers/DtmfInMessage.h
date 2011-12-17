3cab941df4f75cc6ad7ec3d0c083f2eace3d13e8
Rudi Hansen <dimx@dimx.dk>
2011-12-17 17:43:59 +0100

    DtmfInMessage updated

--- src/buffers/DtmfInMessage.h
+++ src/buffers/DtmfInMessage.h
@@ -1,17 +1,18 @@
 #ifndef DTMFINMESSAGE_H
 #define DTMFINMESSAGE_H
 
+#include <cstring>
+
 class DtmfInMessage{
 protected:
 	unsigned char senderAddress_;
 	unsigned char senderPort_;
 	unsigned long dataLength_;
 	unsigned char * data_;
-public:
-	DtmfInMessage();
 	DtmfInMessage(unsigned char senderAddress, unsigned char senderPort, unsigned long dataLength, unsigned char * data);
 	~DtmfInMessage();
-    unsigned char getSenderAdress();
+public:
+    unsigned char getSenderAddress();
 	void getData(unsigned char *& data, unsigned long startAddress, unsigned long stopAddress);
 	void getData(unsigned char *& data);
 	unsigned long getMessageLength();
